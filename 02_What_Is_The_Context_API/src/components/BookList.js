27a09a2b46580b5036886ba3a52713d6e2eaf7f4
daniel ouattara <daniel.ouattara@gmx.com>
2021-11-01 02:39:48 +0100

    tutorail 1 & 2 commit

---
+++ 02_What_Is_The_Context_API/src/components/BookList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class BookList extends Component {
+    render() {
+        return (
+            <div className="bookList">
+                <ul>
+                    <li>The way of King</li>
+                    <li>The name of the wind</li>
+                    <li>The final empire</li>
+                </ul>
+                
+            </div>
+        )
+    }
+}
