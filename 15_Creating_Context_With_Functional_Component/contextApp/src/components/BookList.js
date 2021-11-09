a9bac29f2da886e9733b6c260135d9ea69d05a65
daniel ouattara <daniel.ouattara@gmx.com>
2021-11-09 21:20:35 +0100

    14 -corrections + 15

---
+++ 15_Creating_Context_With_Functional_Component/contextApp/src/components/BookList.js
@@ -0,0 +1,22 @@
+
+import React, { useContext } from 'react'
+import { ThemeContext } from '../contexts/ThemeContext'
+
+const Booklist = () => {
+    const {isLightTheme, light, dark} = useContext(ThemeContext);
+    // const { isLightTheme, light, dark } = this.context;  // NOT POSSIBLE IN FUNCTION COMPONENT
+    const theme = isLightTheme ? light : dark;
+    return ( 
+        <div 
+            className="book-list" 
+            style= {{background: theme.bg, color: theme.syntax}} >
+            <ul>
+                <li style= {{background: theme.ui}}>The way of King</li>
+                <li style= {{background: theme.ui}}>The name of the wind</li>
+                <li style= {{background: theme.ui}}>The final empire</li>
+            </ul> 
+        </div>
+     );
+}
+ 
+export default Booklist;
