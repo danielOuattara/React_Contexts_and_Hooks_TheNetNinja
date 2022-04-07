d6f32d227cc3e9cc921e3162e76c2dc6bb1a9ab7
daniel ouattara <daniel.ouattara@gmx.com>
2022-04-07 10:22:56 +0200

    clean up

--- 03_Adding_Context_and_Provider/contextApp/src/contexts/ThemeContext.js
+++ 03_Adding_Context_and_Provider/contextApp/src/contexts/ThemeContext.js
@@ -12,12 +12,6 @@ class ThemeContextProvider extends Component {
     };
   }
 
-  // state = {
-  //     isLightTheme: true,
-  //     light: { syntax: '#555', ui:'#ddd', bg: '#eee' },
-  //     dark: { syntax: '#ddd', ui:'#333', bg: '#555' },
-  // }
-
   render() {
     return (
       <ThemeContext.Provider value={{ ...this.state }}>
